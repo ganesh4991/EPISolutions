b3488d307b5ff59ad493c5bb751161ff60186cd4
ganesh4991 <ganesh4991@gmail.com>
2020-08-13 21:42:38 -0400

    initial commit for EPI solutions

---
+++ src/com/epi/arrays/DNF.java
@@ -0,0 +1,24 @@
+package com.epi.arrays;
+
+import java.util.Collections;
+import java.util.List;
+
+public class DNF {
+    public List<Integer> solve(List<Integer> input, int pivotIndex) {
+        int low = 0, mid = 0, high = input.size()-1;
+        int pivot = input.get(pivotIndex);
+
+        while (mid <= high) {
+            int currentValue = input.get(mid);
+            if (currentValue < pivot) {
+                Collections.swap(input, low++, mid++);
+            } else if (currentValue > pivot) {
+                Collections.swap(input, high--, mid);
+            } else {
+                mid++;
+            }
+        }
+
+        return input;
+    }
+}
